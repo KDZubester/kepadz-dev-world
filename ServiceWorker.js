94cbd923c5d0f708f1c42702d721602c9fac1f8a
KDZubester <63425135+KDZubester@users.noreply.github.com>
2024-11-12 14:46:09 -0700

    Changed Compression Format to Disabled

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "NextDoorGamer-kepadz-portfolio-website-0.1.0";
+const contentToCache = [
+    "Build/kepadz-dev-world.loader.js",
+    "Build/kepadz-dev-world.framework.js",
+    "Build/kepadz-dev-world.data",
+    "Build/kepadz-dev-world.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
