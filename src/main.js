a93e9fab3dcc8523cb9a31150b0f1130fc6bffa5
KDZubester <63425135+KDZubester@users.noreply.github.com>
2024-12-19 08:06:40 -0700

    Fixed how we imported THREE
    I was using vite in my own development environment, which allowed me to get THREE natively. But github doesn't have that, so I needed to change how we imported THREE

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
+// import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
