0e5b801caee90f2d9a72efee1f857e6c718ccf7b
KDZubester <daniel@zubeldia.us>
2025-02-27 18:20:33 -0800

    Blog Article Database
    Got the blog articles to be created and stored onto a mySQL database

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+    "fmt"
+    "database/sql"
 
 	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var articleTemplate *template.Template
 var blogTemplate *template.Template
+var db *sql.DB
 
 // Article struct
 type Article struct {
@@ -20,9 +24,6 @@ type Article struct {
 	Summary string
 	Content template.HTML
 }
-
-// To hold articles
-var articles []Article
 
 func init() {
 	var err error
@@ -34,27 +35,31 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+    // Initialize database connection using environment variables
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_PORT"),
+        os.Getenv("DB_NAME"))
+    db, err = sql.Open("mysql", dsn)
+    if err != nil {
+        panic(err)
+    }
+
+    // Test the database connection
+    err = db.Ping()
+    if err != nil {
+        panic(err)
+    }
 }
 
 func main() {
-	// Create a few articles with HTML content
-	articles = append(articles, Article{
-		ID:      "1",
-		Title:   "Article 1",
-		Image:   "/static/art/the_little_prince_favicon.jpg",
-		Summary: "Summary of article 1",
-		Content: template.HTML("<p>This is <strong>article 1</strong> content. It contains <a href='#'>links</a> and <em>formatted</em> text.</p>"),
-	})
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", serveIndex)
-	r.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
-		// Pass articles to the blog page
-		err := blogTemplate.ExecuteTemplate(w, "blog", articles)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	r.HandleFunc("/blog", serveBlog)
 	r.HandleFunc("/blog/article/{id}", articleHandler)        // Serve article dynamically
 	r.HandleFunc("/blog/createArticle", serveCreateArticle)   // Serve the article creation form
 	r.HandleFunc("/saveArticle", saveArticle).Methods("POST") // Save the new article
@@ -83,6 +88,31 @@ func servePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, page)
 }
 
+func serveBlog(w http.ResponseWriter, r *http.Request) {
+    rows, err := db.Query("SELECT id, title, image, summary, content FROM articles")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
+
+    var articles []Article
+    for rows.Next() {
+        var article Article
+        err := rows.Scan(&article.ID, &article.Title, &article.Image, &article.Summary, &article.Content)
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        articles = append(articles, article)
+    }
+
+    err = blogTemplate.ExecuteTemplate(w, "blog", articles)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
 // Route to show the article creation form
 func serveCreateArticle(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "createArticle.html")
@@ -92,22 +122,19 @@ func serveCreateArticle(w http.ResponseWriter, r *http.Request) {
 func articleHandler(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 
-	// Find article by ID
-	var article *Article
-	for _, a := range articles {
-		if a.ID == id {
-			article = &a
-			break
-		}
-	}
-
-	if article == nil {
-		http.NotFound(w, r)
-		return
-	}
+    // Find article by ID
+    var article Article
+    err := db.QueryRow("SELECT id, title, image, summary, content FROM articles WHERE id = ?", id).Scan(&article.ID, &article.Title, &article.Image, &article.Summary, &article.Content)
+    if err == sql.ErrNoRows {
+        http.NotFound(w, r)
+        return
+    } else if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
 	// Render article page
-	err := articleTemplate.ExecuteTemplate(w, "article", article)
+	err = articleTemplate.ExecuteTemplate(w, "article", article)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -121,17 +148,12 @@ func saveArticle(w http.ResponseWriter, r *http.Request) {
 	summary := r.FormValue("summary")
 	content := r.FormValue("content")
 
-	// Create a new article
-	newArticle := Article{
-		ID:      string(len(articles) + 1), // Create a new ID
-		Title:   title,
-		Image:   image,
-		Summary: summary,
-		Content: template.HTML(content),
-	}
-
-	// Save the article in the articles slice
-	articles = append(articles, newArticle)
+	// Insert the new article into the database
+    _, err := db.Exec("INSERT INTO articles (title, image, summary, content) VALUES (?, ?, ?, ?)", title, image, summary, content)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 
 	// Redirect to the blog page
 	http.Redirect(w, r, "/blog", http.StatusSeeOther)
